168563567f57e2ffb1f887ed00f94e37ea87249b
Daniel Lomelino <daniel.lomelino@kenzie.academy>
2020-04-26 20:18:04 -0400

    Update for consistency

--- mimic.py
+++ mimic.py
@@ -5,52 +5,28 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
-"""
-Google's Python Class
+"""Mimic exercise
 
 Read in the file specified on the command line.
 Do a simple split() on whitespace to obtain all the words in the file.
-Rather than read the file line by line, it's easier to read
-it into one giant string and split it once.
+Rather than read the file line by line, it's easier to read it into
+one giant string and split it once.
 
-Build a "mimic" dict that maps each word that appears in the file
-to a list of all the words that immediately follow that word in the file.
-The list of words can be be in any order and should include
-duplicates. So for example the key "and" might have the list
-["then", "best", "then", "after", ...] listing
-all the words which came after "and" in the text.
-We'll say that the empty string "" is what comes before
-the first word in the file.  This will be the seed string.
+Note: the standard python module 'random' includes a random.choice(list)
+method which picks a random element from a non-empty list.
 
-With the mimic dict, it's fairly easy to emit random
-text that mimics the original. Print a word, then look
-up what words might come next and pick one at random as
-the next word.
-Use the empty string as the first word to prime things.
-If we ever get stuck with a word that is not in the dict,
-go back to the empty string to keep things moving.
-
-Note: the standard python module 'random' includes a
-random.choice(list) method which picks a random element
-from a non-empty list.
-
-For fun, feed your program to itself as input.
-Could work on getting it to put in linebreaks around 70
-columns, so the output looks better.
-
+You can try adding in line breaks around 70 columns so the output looks
+better.
 """
+
+__author__ = "???"
 
 import random
 import sys
 
-__author__ = "???"
-
 
 def create_mimic_dict(filename):
-    """Returns mimic dict mapping each word to list of words which follow it.
+    """Returns a dict mapping each word to a list of words which follow it.
     For example:
         Input: "I am a software developer, and I don't care who knows"
         Output:
@@ -68,12 +44,14 @@ def create_mimic_dict(filename):
             }
     """
     # +++your code here+++
+    pass
 
 
 def print_mimic(mimic_dict, start_word):
-    """Given a previously compiled mimic_dict and start_word, prints 200 random words:
+    """Given a previously created mimic_dict and start_word,
+    prints 200 random words from mimic_dict as follows:
         - Print the start_word
-        - Lookup the start_word in your mimic_dict and get it's next-list
+        - Look up the start_word in your mimic_dict and get its next-list
         - Randomly select a new word from the next-list
         - Repeat this process 200 times
     """
@@ -81,10 +59,10 @@ def print_mimic(mimic_dict, start_word):
     pass
 
 
-# Provided main(), calls mimic_dict() and mimic()
+# Provided main(), calls mimic_dict() and print_mimic()
 def main():
     if len(sys.argv) != 2:
-        print 'usage: python mimic.py file-to-read'
+        print('usage: python mimic.py file-to-read')
         sys.exit(1)
 
     d = create_mimic_dict(sys.argv[1])
